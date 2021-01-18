3b3a541f6a52a8518cf7574a4b98d24358e77cff
Daniel Amsel <djamsel@lucidchart.com>
2021-01-18 13:00:58 -0700

    Inital prettier format

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,7 @@
 import './App.css';
-import React from "react";
 
 function App() {
-  let me
-  
-  return (
-    <div className="app">
-    </div>
-  );
+    return <div className="app"></div>;
 }
 
 export default App;
