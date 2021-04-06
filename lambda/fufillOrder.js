921dbd59ed9371cf7798ffa63d78af1a5ade1ae6
Daniel Amsel <lesmaxleinad@gmail.com>
2021-04-05 22:23:50 -0600

    Update lambda with type info

--- lambda/fufillOrder.js
+++ lambda/fufillOrder.js
@@ -1,21 +1,48 @@
+/**
+ * @type import('stripe').Stripe
+ * */
 const stripe = require('stripe')(process.env.STRIPE_API_SECRET);
 
-exports.handler = async function (event, context) {
-    const lineItemsData = await stripe.checkout.sessions.listLineItems(
-        JSON.parse(event.body)['data']['object']['id']
-    );
-
-    console.log('--- PRINT LINE ITEMS ---');
-    console.log(lineItemsData);
-    console.log('_________________________');
-
-    const lineItems = lineItemsData['data'];
-
-    lineItems.forEach((lineItem) => {
-        console.log('Nickname: ', lineItem.price.nickname);
-        console.log('Quantity: ', lineItem.quantity);
-    });
-    return {
+const STATUS_CODE = {
+    OK: {
         statusCode: 200,
-    };
+    },
 };
+
+/**
+ * @param {import('aws-lambda').APIGatewayEvent} event
+ * @param {import('aws-lambda').Context} context
+ * */
+async function handler(event, context) {
+    try {
+        const body = JSON.parse(event.body) || {};
+        const data = body['data'] || {};
+        const object = data['object'] || {};
+        const sessionId = object['id'];
+        if (sessionId) {
+            const lineItemsData = await stripe.checkout.sessions.listLineItems(
+                sessionId
+            );
+            console.log('--- PRINT LINE ITEMS ---');
+            console.log(lineItemsData);
+            console.log('_________________________');
+
+            const lineItems = lineItemsData['data'];
+
+            lineItems.forEach((lineItem) => {
+                console.log('Nickname: ', lineItem.price.nickname);
+                console.log('Quantity: ', lineItem.quantity);
+            });
+            return STATUS_CODE.OK;
+        } else {
+            throw new Error('No SessionID found on the event object!');
+        }
+    } catch (e) {
+        console.error(e);
+        console.error(event);
+        console.error(context);
+        return STATUS_CODE.OK;
+    }
+}
+
+exports.handler = handler;
